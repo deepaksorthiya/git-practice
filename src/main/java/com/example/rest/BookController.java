a45693bc429425899b0ecd458c3ed97281920aec
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-03-28 22:42:36 +0530

    develop commit

--- src/main/java/com/example/rest/BookController.java
+++ src/main/java/com/example/rest/BookController.java
@@ -2,12 +2,16 @@ package com.example.rest;
 
 import com.example.entity.Book;
 import com.example.repository.BookRepository;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -15,16 +19,25 @@ import java.util.Optional;
 public class BookController {
     private final BookRepository bookRepository;
     private final Environment environment;
+    private final WebMvcEndpointHandlerMapping webMvcEndpointHandlerMapping;
 
-    public BookController(BookRepository bookRepository, Environment environment) {
+    public BookController(BookRepository bookRepository, WebMvcEndpointHandlerMapping webMvcEndpointHandlerMapping, Environment environment) {
         this.bookRepository = bookRepository;
         this.environment = environment;
+        this.webMvcEndpointHandlerMapping = webMvcEndpointHandlerMapping;
     }
 
     @GetMapping("/envars")
     public ResponseEntity<String[]> getEnvs() {
         String[] profiles = this.environment.getActiveProfiles();
         return ResponseEntity.ok(profiles);
+    }
+
+
+    @GetMapping("/envars1")
+    public ResponseEntity<Map<RequestMappingInfo, HandlerMethod>> getEnvs1() {
+        Map<RequestMappingInfo, HandlerMethod> methods = webMvcEndpointHandlerMapping.getHandlerMethods();
+        return ResponseEntity.ok(methods);
     }
 
     @GetMapping
